fe0b1ada71412fea80cf2fcd9867025dbfe8f3a4
shayolina <yasinnmoosavi@gmail.com>
2020-05-12 15:06:26 +0430

    SellerMenu completed

--- src/main/java/Menus/SellerMenu.java
+++ src/main/java/Menus/SellerMenu.java
@@ -12,6 +12,21 @@ public class SellerMenu extends Menu{
         super(name, subMenu);
     }
 
+    @Override
+    protected void show() {
+
+        for(int i=0 ; i < subMenu.size() ; i++) {
+            System.out.println("" + i + "." + subMenu.get(i));
+        }
+        System.out.println(""+ subMenu.size() + ".back");
+        System.out.println("Please select a number:");
+    }
+
+    @Override
+    protected void execute() {
+        super.execute();
+    }
+
     public void setUser(Seller user) {
         this.user = user;
     }
