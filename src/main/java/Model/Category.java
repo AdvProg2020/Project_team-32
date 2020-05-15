e4432614e9e455cbc1c0bc2daeaac7d65e4506b3
muzzi56 <62210278+muzzi56@users.noreply.github.com>
2020-05-15 23:40:26 +0430

    :)

--- src/main/java/Model/Category.java
+++ src/main/java/Model/Category.java
@@ -5,22 +5,18 @@ import java.util.HashMap;
 
 public class Category {
 
-    private static ArrayList<Category> allCategories = new ArrayList<Category>();
-
+    public static Category rootCategory = new Category("mainCategory", null , null);
+    private static ArrayList<Category> allCategories = new ArrayList<>();
     private String name;
     private Category parentCategory;
 
     /*
     //----------------------------------------------------------------------------
     // by ali sharifi
-
     private static HashMap<String , String> generalProperties = new HashMap<String, String>();
-
     //----------------------------------------------------------------------------
-
     private HashMap<String, String> specialProperties; // key is property name and value is property amount
      */
-
 
     private static ArrayList<String> generalProperties;
     static {
@@ -30,15 +26,15 @@ public class Category {
         generalProperties.add("seller");
     }
     private ArrayList<String> specialProperties;
-    public Category(String name, ArrayList<String> specialProperty) {
+    public Category(String name, ArrayList<String> specialProperty, Category parentCategory) {
         this.name = name;
         this.specialProperties = specialProperty;
+        this.parentCategory = parentCategory;
         allCategories.add(this);
     }
 
-
-    private ArrayList<Category> subCategory = new ArrayList<Category>();
-    private ArrayList<Good> allGoods;
+    private ArrayList<Category> subCategory = new ArrayList<>();
+    private ArrayList<Good> categoryProduct;
 
     public String getName() {
         return name;
@@ -51,10 +47,9 @@ public class Category {
     }
     */
 
-    public void addCategory(Category category) {
+    public void addSubCategory(Category category) {
         this.subCategory.add(category);
     }
-
 
     public static ArrayList<Category> getAllCategories() {
         return allCategories;
@@ -66,5 +61,21 @@ public class Category {
 
     public ArrayList<String> getSpecialProperties() {
         return specialProperties;
+    }
+
+    public ArrayList<Good> getCategoryProduct() {
+        return categoryProduct;
+    }
+
+    public ArrayList<Category> getSubCategory() {
+        return subCategory;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpecialProperties(ArrayList<String> specialProperties) {
+        this.specialProperties = specialProperties;
     }
 }
