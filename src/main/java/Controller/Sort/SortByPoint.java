91a5c5b734d5e9aa61145911df5911969e1364e6
SHARIFI <a.sharifi1379@gmail.com>
2020-05-19 13:26:28 +0430

    sortByPoint edited

--- src/main/java/Controller/Sort/SortByPoint.java
+++ src/main/java/Controller/Sort/SortByPoint.java
@@ -8,7 +8,16 @@ public class SortByPoint implements Comparator<Good> {
 
     @Override
     public int compare(Good good, Good t1) {
-        return good.getPoint() - t1.getPoint();
+        //return good.getPoint() - t1.getPoint();
+
+        if(good.getPoint() > t1.getPoint()){
+            return 1;
+        } else if( good.getPoint() == t1.getPoint()){
+            return 0;
+        } else {
+            return -1;
+        }
+
     }
     
 }
