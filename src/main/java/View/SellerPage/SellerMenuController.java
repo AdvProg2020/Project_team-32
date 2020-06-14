31eebced4f27aec0639fb543d059c5073ae7fb34
shayolina <yasinnmoosavi@gmail.com>
2020-06-14 17:24:54 +0430

    Lable mistake fixed

--- src/main/java/View/SellerPage/SellerMenuController.java
+++ src/main/java/View/SellerPage/SellerMenuController.java
@@ -67,7 +67,7 @@ public class SellerMenuController implements Initializable {
 
 
     @FXML
-    TableView tableView ;
+    TableView tableView;
 
 
     @FXML
@@ -89,27 +89,27 @@ public class SellerMenuController implements Initializable {
     @FXML
     Button editProduct;
     @FXML
-    Label productID;
+    TextField productID;
 
     @FXML
     TableView<String> categoryTable;
     @FXML
-    TableColumn<String,String> categoryColumn;
+    TableColumn<String, String> categoryColumn;
     @FXML
     Label balanceLable;
 
     @FXML
-    Label addProductID_Label;
+    TextField addProductID_Label;
     @FXML
     Button addProductID_Button;
     @FXML
-    Pane addProductPane ;
+    Pane addProductPane;
 
 
     @FXML
     Label messageRemoveProduct;
     @FXML
-    Label IdRemoveProduct;
+    TextField IdRemoveProduct;
     @FXML
     Button RemoveProductButton;
 
@@ -118,11 +118,10 @@ public class SellerMenuController implements Initializable {
         //info personal
 
 
+        //company name
+        companyNameLabel.setText(((Seller) AccountController.loggedInUser).getFactoryName());
 
-            //company name
-        companyNameLabel.setText(((Seller)AccountController.loggedInUser).getFactoryName());
-
-            // sales history
+        // sales history
         tableColumn1.setCellValueFactory(new PropertyValueFactory<>("logID"));
         tableColumn2.setCellValueFactory(new PropertyValueFactory<>("buyerUserNmae"));
         tableColumn3.setCellValueFactory(new PropertyValueFactory<>("pricePaid"));
@@ -135,12 +134,12 @@ public class SellerMenuController implements Initializable {
         //manage product begening
         for (Good sellingGood : ((Seller) AccountController.loggedInUser).getSellingGoods()) {
             GridPane gridPane = new GridPane();
-            Label name =new Label();
+            Label name = new Label();
             Label ID = new Label();
             name.setText(sellingGood.getName());
             ID.setText(sellingGood.getGoodID());
-            gridPane.add(name,0,0);
-            gridPane.add(ID,1,0);
+            gridPane.add(name, 0, 0);
+            gridPane.add(ID, 1, 0);
             manageProductPane.getChildren().add(gridPane);
         }
 
@@ -149,13 +148,13 @@ public class SellerMenuController implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 try {
-                    Good good = SellerController.viewProduct(((Seller)AccountController.loggedInUser),productID.getText());
+                    Good good = SellerController.viewProduct(((Seller) AccountController.loggedInUser), productID.getText());
                     TableView productTable = new TableView();
-                    TableColumn<String,Good> goodName = new TableColumn<>("good name");
-                    TableColumn<String,Good> goodId = new TableColumn<>("good ID");
-                    TableColumn<String,Good> point = new TableColumn<>("point");
-                    TableColumn<String,Good> categoryName = new TableColumn<>("category name");
-                    TableColumn<String,Good> explanations = new TableColumn<>("explanations");
+                    TableColumn<String, Good> goodName = new TableColumn<>("good name");
+                    TableColumn<String, Good> goodId = new TableColumn<>("good ID");
+                    TableColumn<String, Good> point = new TableColumn<>("point");
+                    TableColumn<String, Good> categoryName = new TableColumn<>("category name");
+                    TableColumn<String, Good> explanations = new TableColumn<>("explanations");
                     goodName.setCellValueFactory(new PropertyValueFactory<>("name"));
                     goodId.setCellValueFactory(new PropertyValueFactory<>("goodID"));
                     categoryName.setCellValueFactory(new PropertyValueFactory<>("categoryString"));
@@ -176,11 +175,11 @@ public class SellerMenuController implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 try {
-                    ArrayList<String> allBuyers = SellerController.viewProductBuyers(((Seller)AccountController.loggedInUser),productID.getText());
+                    ArrayList<String> allBuyers = SellerController.viewProductBuyers(((Seller) AccountController.loggedInUser), productID.getText());
 
                     ObservableList<String> details = FXCollections.observableArrayList(allBuyers);
                     TableView<String> buyersTable = new TableView<>();
-                    TableColumn<String,String> col1 = new TableColumn<>("Buyers");
+                    TableColumn<String, String> col1 = new TableColumn<>("Buyers");
                     buyersTable.getColumns().addAll(col1);
                     col1.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
                     buyersTable.setItems(details);
@@ -199,26 +198,26 @@ public class SellerMenuController implements Initializable {
             public void handle(ActionEvent event) {
 
                 try {
-                    Good good = SellerController.getGoodFromSellingGood(((Seller) AccountController.loggedInUser),productID.getText());
-                    Label categoryLabel = new Label("category name");
+                    Good good = SellerController.getGoodFromSellingGood(((Seller) AccountController.loggedInUser), productID.getText());
+                    TextField categoryTextField = new TextField("category name");
                     Button setCategory = new Button("press to set category");
                     manageProductPane.getChildren().clear();
-                    manageProductPane.getChildren().addAll(categoryLabel,setCategory);
+                    manageProductPane.getChildren().addAll(categoryTextField, setCategory);
                     setCategory.setOnAction(new EventHandler<ActionEvent>() {
                         @Override
                         public void handle(ActionEvent event) {
 
                             try {
-                                Category category = CategoryController.getCategoryByName(categoryLabel.getText());
-                                Label goodName = new Label("good name");
-                                Label price = new Label("price");
-                                Label companyName = new Label("company name");
-                                Label explanation = new Label("explanation");
-                                Label[] labelProperties =  new Label[category.getSpecialProperties().size()];
-                                HashMap<String,String> properties = new HashMap<>();
-                                int i=0;
+                                Category category = CategoryController.getCategoryByName(categoryTextField.getText());
+                                TextField goodName = new TextField("good name");
+                                TextField price = new TextField("price");
+                                TextField companyName = new TextField("company name");
+                                TextField explanation = new TextField("explanation");
+                                TextField[] textFieldProperties = new TextField[category.getSpecialProperties().size()];
+                                HashMap<String, String> properties = new HashMap<>();
+                                int i = 0;
                                 for (String specialProperty : category.getSpecialProperties()) {
-                                    labelProperties[i] = new Label(specialProperty);
+                                    textFieldProperties[i] = new TextField(specialProperty);
                                     i++;
                                 }
                                 Button confirm = new Button("confirm");
@@ -226,19 +225,19 @@ public class SellerMenuController implements Initializable {
                                     @Override
                                     public void handle(ActionEvent event) {
 
-                                        int i=0;
+                                        int i = 0;
                                         for (String specialProperty : category.getSpecialProperties()) {
-                                            properties.put(specialProperty,labelProperties[i].getText().trim());
+                                            properties.put(specialProperty, textFieldProperties[i].getText().trim());
                                             i++;
                                         }
-                                        GoodController.editProduct(good,goodName.getText().trim(),companyName.getText().trim(),Integer.parseInt(price.getText().trim())
-                                                , ((Seller)AccountController.loggedInUser) ,explanation.getText().trim(),category,properties);
+                                        GoodController.editProduct(good, goodName.getText().trim(), companyName.getText().trim(), Integer.parseInt(price.getText().trim())
+                                                , ((Seller) AccountController.loggedInUser), explanation.getText().trim(), category, properties);
                                         System.out.println("Request sent successfully.");
                                     }
                                 });
-                                manageProductPane.getChildren().addAll(goodName,price,companyName,explanation);
-                                for (int j=0;j<category.getSpecialProperties().size();j++){
-                                    manageProductPane.getChildren().add(labelProperties[j]);
+                                manageProductPane.getChildren().addAll(goodName, price, companyName, explanation);
+                                for (int j = 0; j < category.getSpecialProperties().size(); j++) {
+                                    manageProductPane.getChildren().add(textFieldProperties[j]);
                                 }
 
 
@@ -250,7 +249,7 @@ public class SellerMenuController implements Initializable {
                     });
 
 
-                } catch (InvalidIDException  e) {
+                } catch (InvalidIDException e) {
                     e.printStackTrace();
                 }
 
@@ -264,44 +263,44 @@ public class SellerMenuController implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 addProductPane.getChildren().clear();
-                Good good_addProduct =Good.getGoodFromAllGoods(addProductID_Label.getText().trim());
-                if(good_addProduct!=null){
+                Good good_addProduct = Good.getGoodFromAllGoods(addProductID_Label.getText().trim());
+                if (good_addProduct != null) {
                     System.out.println("the id is already exist enter a price to add as its seller or 0 to ignore it:");
-                    Label priceLabel = new Label("enter new price");
+                    TextField priceTextField = new TextField("enter new price");
                     Button confirmPrice = new Button("confirm price");
                     confirmPrice.setOnMouseClicked(new EventHandler<MouseEvent>() {
                         @Override
                         public void handle(MouseEvent event) {
-                            int price = Integer.parseInt(priceLabel.getText().trim());
-                            if(price > 0){
-                                good_addProduct.addSellerAndPrice(AccountController.loggedInUser.getUserName(),price);
-                                priceLabel.setText("succesful");
+                            int price = Integer.parseInt(priceTextField.getText().trim());
+                            if (price > 0) {
+                                good_addProduct.addSellerAndPrice(AccountController.loggedInUser.getUserName(), price);
+                                priceTextField.setText("succesful");
                             }
                         }
                     });
-                    addProductPane.getChildren().addAll(priceLabel,confirmPrice);
+                    addProductPane.getChildren().addAll(priceTextField, confirmPrice);
 
-                }else {
+                } else {
 
-                    Label categoryLabel = new Label("category name");
+                    TextField categoryTextField = new TextField("category name");
                     Button setCategory = new Button("press to set category");
                     addProductPane.getChildren().clear();
-                    addProductPane.getChildren().addAll(categoryLabel,setCategory);
+                    addProductPane.getChildren().addAll(categoryTextField, setCategory);
                     setCategory.setOnAction(new EventHandler<ActionEvent>() {
                         @Override
                         public void handle(ActionEvent event) {
 
                             try {
-                                Category category = CategoryController.getCategoryByName(categoryLabel.getText());
-                                Label goodName = new Label("good name");
-                                Label price = new Label("price");
-                                Label companyName = new Label("company name");
-                                Label explanation = new Label("explanation");
-                                Label[] labelProperties =  new Label[category.getSpecialProperties().size()];
-                                HashMap<String,String> properties = new HashMap<>();
-                                int i=0;
+                                Category category = CategoryController.getCategoryByName(categoryTextField.getText());
+                                TextField goodName = new TextField("good name");
+                                TextField price = new TextField("price");
+                                TextField companyName = new TextField("company name");
+                                TextField explanation = new TextField("explanation");
+                                TextField[] textFieldProperties = new TextField[category.getSpecialProperties().size()];
+                                HashMap<String, String> properties = new HashMap<>();
+                                int i = 0;
                                 for (String specialProperty : category.getSpecialProperties()) {
-                                    labelProperties[i] = new Label(specialProperty);
+                                    textFieldProperties[i] = new TextField(specialProperty);
                                     i++;
                                 }
                                 Button confirm = new Button("confirm");
@@ -309,21 +308,21 @@ public class SellerMenuController implements Initializable {
                                     @Override
                                     public void handle(ActionEvent event) {
 
-                                        int i=0;
+                                        int i = 0;
                                         for (String specialProperty : category.getSpecialProperties()) {
-                                            properties.put(specialProperty,labelProperties[i].getText().trim());
+                                            properties.put(specialProperty, textFieldProperties[i].getText().trim());
                                             i++;
                                         }
-                                        GoodController.AddProduct(addProductID_Label.getText().trim(),goodName.getText().trim()
-                                                ,companyName.getText().trim(),Integer.parseInt(price.getText().trim()),
-                                                explanation.getText().trim(),properties,((Seller)AccountController.loggedInUser)
-                                                ,category);
+                                        GoodController.AddProduct(addProductID_Label.getText().trim(), goodName.getText().trim()
+                                                , companyName.getText().trim(), Integer.parseInt(price.getText().trim()),
+                                                explanation.getText().trim(), properties, ((Seller) AccountController.loggedInUser)
+                                                , category);
                                         System.out.println("Request sent successfully.");
                                     }
                                 });
-                                addProductPane.getChildren().addAll(goodName,price,companyName,explanation);
-                                for (int j=0;j<category.getSpecialProperties().size();j++){
-                                    addProductPane.getChildren().add(labelProperties[j]);
+                                addProductPane.getChildren().addAll(goodName, price, companyName, explanation);
+                                for (int j = 0; j < category.getSpecialProperties().size(); j++) {
+                                    addProductPane.getChildren().add(textFieldProperties[j]);
                                 }
 
 
@@ -335,11 +334,9 @@ public class SellerMenuController implements Initializable {
                     });
 
 
-
                 }
             }
         });
-
 
 
         //remove product
@@ -347,7 +344,7 @@ public class SellerMenuController implements Initializable {
             @Override
             public void handle(MouseEvent event) {
                 try {
-                    SellerController.removeProduct(((Seller)AccountController.loggedInUser),IdRemoveProduct.getText().trim());
+                    SellerController.removeProduct(((Seller) AccountController.loggedInUser), IdRemoveProduct.getText().trim());
                     messageRemoveProduct.setText("succesfully deleted the product");
                 } catch (InvalidIDException e) {
                     e.printStackTrace();
@@ -358,17 +355,16 @@ public class SellerMenuController implements Initializable {
 
 
         //show categories
-        ArrayList<Category> categories =SellerController.showCategory(((Seller)AccountController.loggedInUser));
-        ArrayList<String> categoryNames =new ArrayList<>();
+        ArrayList<Category> categories = SellerController.showCategory(((Seller) AccountController.loggedInUser));
+        ArrayList<String> categoryNames = new ArrayList<>();
         for (Category category : categories) {
             categoryNames.add(category.getName());
         }
-        ObservableList<String> details = FXCollections.observableArrayList(categoryNames) ;
+        ObservableList<String> details = FXCollections.observableArrayList(categoryNames);
         categoryColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
         categoryTable.setItems(details);
 
         //view offs
-
 
 
         //all tabs selected initializing
@@ -380,28 +376,11 @@ public class SellerMenuController implements Initializable {
 
                 //view balance
 
-                if(newTab.equals (balanceTab)) {
-                    balanceLable.setText("remaining money :"+((Seller)AccountController.loggedInUser).getCredit()+"Tomans");
+                if (newTab.equals(balanceTab)) {
+                    balanceLable.setText("remaining money :" + ((Seller) AccountController.loggedInUser).getCredit() + "Tomans");
                 }
             }
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
