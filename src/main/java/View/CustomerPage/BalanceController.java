993fd69b2f1f6c91524edeafc400eed531d2d143
shayolina <yasinnmoosavi@gmail.com>
2020-06-25 01:00:21 +0430

    customer balance added

--- src/main/java/View/CustomerPage/BalanceController.java
+++ src/main/java/View/CustomerPage/BalanceController.java
@@ -1,4 +1,20 @@
 package View.CustomerPage;
 
-public class BalanceController {
+import Controller.AccountController;
+import Model.Customer;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BalanceController implements Initializable {
+
+    @FXML
+    private Label balance;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        balance.setText(String.valueOf(((Customer) AccountController.loggedInUser).getCredit()));
+    }
 }
