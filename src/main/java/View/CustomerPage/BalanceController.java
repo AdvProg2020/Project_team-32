510d012f46180b3806542128f1b6d072920ceb80
shayolina <yasinnmoosavi@gmail.com>
2020-06-27 10:55:41 +0430

    add money for customer done

--- src/main/java/View/CustomerPage/BalanceController.java
+++ src/main/java/View/CustomerPage/BalanceController.java
@@ -3,11 +3,15 @@ package View.CustomerPage;
 import Controller.AccountController;
 import Controller.Controller;
 import Model.Customer;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 
@@ -22,10 +26,24 @@ public class BalanceController implements Initializable {
     private ImageView image;
     @FXML
     private Label balance;
+
+    @FXML
+    private TextField textBalance;
+
+    @FXML
+    private Button applyButton;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         balance.setOnMouseEntered(event -> Controller.sound(1));
         balance.setText(String.valueOf(((Customer) AccountController.loggedInUser).getCredit()));
+        applyButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                ((Customer)AccountController.loggedInUser).setCredit(((Customer) AccountController.loggedInUser).getCredit()+Float.parseFloat(textBalance.getText()));
+                balance.setText(String.valueOf(((Customer) AccountController.loggedInUser).getCredit()));
+                System.out.println("wdhaifudbj");
+            }
+        });
     }
 }
